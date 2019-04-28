fcc1a7fbf08df72eb835946900a1670cdfd01605
vMeph <arduino328p@gmail.com>
2019-04-28 01:44:10 +0100

    Update install_commands.sh

--- install_commands.sh
+++ install_commands.sh
@@ -4,4 +4,4 @@ apt-get install -y apparmor-utils apt-transport-https avahi-daemon ca-certificat
 mkdir -p /etc/docker
 echo '{"dns": ["8.8.8.8", "8.8.4.4"]}' > /etc/docker/daemon.json
 curl -sSL https://get.docker.com | sh
-curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install" | bash -s
+curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh" | bash -s
